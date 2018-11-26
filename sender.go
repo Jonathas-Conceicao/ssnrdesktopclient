26b3ac3f0c9120d657649cea9882d6b1894bccd0
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-11-26 15:24:46 -0200

    Basic command line interface

--- sender.go
+++ sender.go
@@ -3,27 +3,77 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
+	"strconv"
 
 	ssnr "github.com/Jonathas-Conceicao/ssnrgo"
 )
 
 func main() {
-	testListing()
-	testMessage()
+	l := len(os.Args)
+	if l < 2 {
+		printHelp()
+		return
+	}
+
+	switch os.Args[1] {
+
+	case "list":
+		if l != 2 {
+			printHelp()
+			return
+		}
+		requestUsers()
+
+	case "send":
+		switch l {
+		case 4:
+			target, err := strconv.ParseInt(os.Args[2], 0, 16)
+			if err != nil {
+				printHelp()
+				return
+			}
+			sendMessage(uint16(target), nil, &os.Args[3])
+		case 5:
+			target, err := strconv.ParseInt(os.Args[2], 0, 16)
+			if err != nil {
+				printHelp()
+				return
+			}
+			sendMessage(uint16(target), &os.Args[3], &os.Args[4])
+		default:
+			printHelp()
+			return
+		}
+
+	default:
+		printHelp()
+	}
 }
 
-func testMessage() {
+// TODO: Add help message for sender
+func printHelp() {
+	helpMessage := "No help message just yet"
+	fmt.Println(helpMessage)
+}
+
+func sendMessage(recv uint16, sndr *string, content *string) {
 	cn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		panic("Failed to dial host")
 	}
 
-	message := ssnr.TestNotification()
+	var message *ssnr.Notification
+	if sndr == nil {
+		message = ssnr.NewAnonymousNotification(recv, *content)
+	} else {
+		message = ssnr.NewNotification(recv, *sndr, *content)
+	}
 	cn.Write(message.Encode())
 	fmt.Println("Message sent!")
 }
 
-func testListing() {
+func requestUsers() {
 	cn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		panic("Failed to dial host")
